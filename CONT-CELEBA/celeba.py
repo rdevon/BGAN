1015a874d5a312b3bfc09e10bab54b90d9d81204
Devon Hjelm <erroneus@gmail.com>
2017-05-17 12:01:48 -0400

    adding naming

--- CONT-CELEBA/celeba.py
+++ CONT-CELEBA/celeba.py
@@ -250,7 +250,8 @@ def BGAN(fake_out, real_out, log_Z=None, use_log_Z=True):
 def LSGAN(fake_out, real_out, target=1.0):
     logger.info('Generator target is {}'.format(target))
     generator_loss = ((fake_out - target) ** 2).mean()
-    discriminator_loss = 0.5 * ((real_out - 1.) ** 2).mean() + 0.5 * (fake_out ** 2).mean()
+    discriminator_loss = 0.5 * ((real_out - 1.) ** 2).mean() + 0.5 * (
+        fake_out ** 2).mean()
     return generator_loss, discriminator_loss
 
 
@@ -328,7 +329,7 @@ def main(data_args, optimizer_args, model_args, train_args,
     
     if log_Z_est is not None:
         generator_updates.update(
-            [(log_Z, 0.995 * log_Z + 0.005 * log_Z_est.mean())])
+            [(log_Z, 0.95 * log_Z + 0.05 * log_Z_est.mean())])
 
     d_results = {
         'p(real)': (real_out > 0.).mean(),
@@ -358,7 +359,8 @@ def main(data_args, optimizer_args, model_args, train_args,
     # TRAIN
     logger.info('Starting training of GAN...')
     total_results = {}
-
+    exp_name = binary_dir.split('/')[-2]
+    
     for epoch in range(train_args['epochs']):
         logger.info('Epoch: '.format(epoch))
         u = 0
@@ -366,7 +368,7 @@ def main(data_args, optimizer_args, model_args, train_args,
         prefix = 'ep_{}'.format(epoch)
         
         results = {}
-        widgets = ['Epoch {}, '.format(epoch), Timer(), Bar()]
+        widgets = ['Epoch {} ({}), '.format(epoch, exp_name), Timer(), Bar()]
         pbar = ProgressBar(
             widgets=widgets,
             maxval=(training_samples // data_args['batch_size'])).start()
@@ -489,7 +491,7 @@ _default_data_args = dict(
 )
 
 _default_optimizer_args = dict(
-    learning_rate=1e-3,
+    learning_rate=1e-4,
     beta1=0.5
 )
 
